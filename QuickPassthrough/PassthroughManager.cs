332efc878cbc9a0b284afeaa0f966199fdd68c35
Angus-W-Fugro <a.warman@fugro.com>
2025-11-03 13:36:11 +0800

    Basic CLI

---
+++ QuickPassthrough/PassthroughManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+
+namespace QuickPassthrough;
+
+public class PassthroughManager : IDisposable
+{
+    private readonly SerialPort _SerialPort;
+    private readonly UdpClient _UdpClient;
+    private byte[] _Buffer = [];
+
+    public PassthroughManager(string incomingCOMPort, string outgoingUDPPort)
+    {
+        _SerialPort = new SerialPort(incomingCOMPort);
+        _SerialPort.Open();
+
+        _UdpClient = new UdpClient();
+        var address = new IPEndPoint(IPAddress.Any, int.Parse(outgoingUDPPort));
+        _UdpClient.Connect(address);
+
+        _SerialPort.DataReceived += SerialPort_DataReceived;
+    }
+
+    public void Dispose()
+    {
+        _SerialPort.DataReceived -= SerialPort_DataReceived;
+
+        _SerialPort.Dispose();
+        _UdpClient.Dispose();
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        var bytesAvailable = _SerialPort.BytesToRead;
+
+        if (_Buffer.Length < bytesAvailable)
+        {
+            Array.Resize(ref _Buffer, bytesAvailable);
+        }
+
+        _SerialPort.Read(_Buffer, 0, bytesAvailable);
+
+        _UdpClient.Send(_Buffer, bytesAvailable);
+    }
+}
--- QuickPassthrough/Program.cs
+++ QuickPassthrough/Program.cs
@@ -1,7 +1,78 @@
-﻿internal class Program
+﻿using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace QuickPassthrough;
+
+internal class Program
 {
+    private const string ConfigFileName = "config.json";
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Run();
+        }
+        catch (Exception ex)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"ERROR: {ex.Message}");
+            Console.ForegroundColor = defaultColor;
+        }
+        finally
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+    private static void Run()
+    {
+        Console.WriteLine("Starting QuickPassthrough\n---");
+
+        if (!File.Exists(ConfigFileName))
+        {
+            Console.WriteLine($"Config file '{ConfigFileName}' not found. Default file will be created. Run the app again to use it.");
+            var defaultConfig = new Config("COM1", "8080");
+            var defaultConfigJson = JsonSerializer.Serialize(defaultConfig, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(ConfigFileName, defaultConfigJson);
+            OpenFile(ConfigFileName);
+            return;
+        }
+
+        var fullPath = Path.GetFullPath(ConfigFileName);
+        Console.WriteLine($"Loading config from: {fullPath}");
+
+        var configJson = File.ReadAllText(ConfigFileName);
+        var config = JsonSerializer.Deserialize<Config>(configJson);
+
+        if (config is null)
+        {
+            throw new InvalidOperationException("Failed to parse config.json");
+        }
+
+        Console.WriteLine("---");
+        Console.WriteLine($" IN: {config.IncomingCOMPort}");
+        Console.WriteLine($"OUT: UDP {config.OutgoingUDPPort}");
+        Console.WriteLine("---");
+
+        var passthroughManager = new PassthroughManager(config.IncomingCOMPort, config.OutgoingUDPPort);
+
+        Console.WriteLine("QuickPassthrough is running. Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    private static void OpenFile(string filePath)
+    {
+        var process = new System.Diagnostics.Process();
+        process.StartInfo.FileName = "notepad.exe";
+        process.StartInfo.Arguments = filePath;
+        process.StartInfo.UseShellExecute = true;
+        process.Start();
     }
 }
+
+public record Config(string IncomingCOMPort, string OutgoingUDPPort);
