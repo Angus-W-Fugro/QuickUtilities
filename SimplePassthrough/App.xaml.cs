742fa90723dce01b8613cd0f179cce9e1a79ceac
Angus-Warman <60087196+Angus-W-Fugro@users.noreply.github.com>
2025-11-04 10:40:56 +0800

    Exception handling

--- SimplePassthrough/App.xaml.cs
+++ SimplePassthrough/App.xaml.cs
@@ -1,14 +1,40 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
-namespace SimplePassthrough
+namespace SimplePassthrough;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public App()
     {
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        DispatcherUnhandledException += App_DispatcherUnhandledException;
+        TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
     }
 
+    private void ShowError(Exception ex)
+    {
+        var message = $"{ex.Message}\n\n{ex.StackTrace}";
+        MessageBox.Show(message, "Error");
+    }
+
+    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        var exception = (Exception)e.ExceptionObject;
+        ShowError(exception);
+    }
+
+    private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        e.Handled = true;
+        ShowError(e.Exception);
+    }
+
+    private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        ShowError(e.Exception);
+    }
 }
